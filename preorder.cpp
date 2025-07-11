9596a9c07b016497058b63c2e0cabb64ce2be65c
dhruv-990 <159527264+dhruv-990@users.noreply.github.com>
2025-07-12 01:10:23 +0530

    gg

--- preorder.cpp
+++ preorder.cpp
@@ -40,6 +40,14 @@ void preorder(Node* root) {
     preorder(root->right);
 }
 
+void inorder(Node *root){
+    if(root==NULL) return;
+
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
 
 
 
